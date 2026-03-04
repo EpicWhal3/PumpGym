3f30a3dc98fa1c2522d07bea149d037300641ea1
EpicWhal3 <max255244@gmail.com>
2026-03-04 16:08:56 +0300

    add: lab-1 impl

---
+++ gym_backend/src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TerminusModule } from '@nestjs/terminus';
+import { HealthController } from './health/health.controller';
+
+@Module({
+  imports: [
+    TerminusModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '../.env'
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      })
+    })
+  ],
+  controllers: [AppController, HealthController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ gym_backend/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }))
+  app.enableCors({
+    origin: [`http://localhost5173`, `http://localhost3000`],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+bootstrap();
