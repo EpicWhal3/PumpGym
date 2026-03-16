683e62b210461da13c8d24c9b74649fc608c238c
EpicWhal3 <max255244@gmail.com>
2026-03-16 10:10:45 +0300

    fix: ESLint fixes

--- gym_backend/src/app.module.ts
+++ gym_backend/src/app.module.ts
@@ -1,32 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { TerminusModule } from '@nestjs/terminus';
-import { HealthController } from './health/health.controller';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TerminusModule } from "@nestjs/terminus";
+import { HealthController } from "./health/health.controller";
 
 @Module({
   imports: [
     TerminusModule,
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '../.env'
+      envFilePath: "../.env",
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USER'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        type: "postgres",
+        host: configService.get("DB_HOST"),
+        port: configService.get<number>("DB_PORT"),
+        username: configService.get("DB_USER"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_NAME"),
         entities: [],
         synchronize: true,
-      })
-    })
+      }),
+    }),
   ],
   controllers: [AppController, HealthController],
   providers: [AppService],
--- gym_backend/src/main.ts
+++ gym_backend/src/main.ts
@@ -1,19 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ transform: true }))
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.enableCors({
     origin: [`http://localhost:3000`],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
-  })
+  });
 
   const port = process.env.PORT || 3000;
   await app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
 }
-bootstrap();
+bootstrap()
+  .then(() => console.log("Bootstrap completed"))
+  .catch((e) => console.error("Bootstrap failed", e));
